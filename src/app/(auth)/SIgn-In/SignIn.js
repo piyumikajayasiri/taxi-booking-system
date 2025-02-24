101e375fa2187df64aee2995d14cbe6082968012
Jayathu SHehan <106993037+JayathuShehan@users.noreply.github.com>
2025-02-24 11:06:52 +0530

    T2K-54 000001 - added sign-in page

---
+++ src/app/(auth)/SIgn-In/SignIn.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  
+
+const SignIn = () => {
+  return (
+    <Card className="w-96 items-center justify-center m-60">
+        <CardHeader>
+            <CardTitle>Sign-In</CardTitle>
+            <CardDescription>please sign in here</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <form>
+                <input type="text" placeholder="Email"/><br></br>
+                <input type="password" placeholder="Password" className='mt-5'/>
+            </form>
+        </CardContent>
+        <CardFooter>
+            <button type="submit" className='w-full bg-black text-white rounded-sm p-1'>Sign In</button>
+        </CardFooter>
+    </Card>
+  )
+}
+
+export default SignIn
